05c7e30dbc11e961949a3bc5bca05ee3acda00c9
bendecre <bed1@umbc.edu>
2022-01-30 17:15:26 -0500

    updated app.js with basic functions

--- Project 1/notes-app/app.js
+++ Project 1/notes-app/app.js
@@ -1,7 +1,46 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.blue('Success!'))
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove commandd
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: function(){
+        console.log('Listing the note')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading the note')
+    }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
