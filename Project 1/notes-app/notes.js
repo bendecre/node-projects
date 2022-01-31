b8464c7750de6306d394496f316d4b718f47611e
bendecre <bed1@umbc.edu>
2022-01-31 06:20:43 -0500

    Refactored code to use arrow functions, added in a list function

--- Project 1/notes-app/notes.js
+++ Project 1/notes-app/notes.js
@@ -1,15 +1,13 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function() {
+const getNotes = () => {
     return "Your notes.."
 }
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if(duplicateNotes.length === 0) {
         notes.push({
@@ -23,11 +21,9 @@ const addNote = function(title, body) {
     }
 }
 
-const removeNote = function(title) {
+const removeNote = (title) => {
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
     if (notes.length > notesToKeep.length) {
         console.log(chalk.green.inverse('Note removed!'))
@@ -38,12 +34,12 @@ const removeNote = function(title) {
     saveNotes(notesToKeep)
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try{
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
@@ -53,8 +49,19 @@ const loadNotes = function () {
     }
 }
 
+const listNotes = () => {
+    console.log(chalk.inverse('Your notes...'))
+
+    const data = loadNotes()
+    
+    data.forEach((entry) => {
+        console.log(entry.title)
+    })
+}
+
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes
 }
