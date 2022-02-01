68eeb97c118df59996e1bf45257f98eb2f0fbf5d
bendecre <bed1@umbc.edu>
2022-02-01 04:20:48 -0500

    Added playground folder in second project

---
+++ Project 2/playground/4-callback.js
@@ -0,0 +1,20 @@
+setTimeout(() => {
+    console.log('Two seconds are up')
+}, 2000)
+
+const names = ['Ben', 'Andrew', 'Jen']
+const shortNames = names.filter((name) => {
+    return name.length <= 4
+})
+
+const geocode = (address, callback) => {
+    const data = {
+        latitude: 0,
+        longitude: 0
+    }
+
+    return data
+}
+
+const data = geocode('Philadelphia')
+console.log(data)
