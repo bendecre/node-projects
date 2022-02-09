889b29375693ebceebdc6ac912936994f02f2965
bendecre <bed1@umbc.edu>
2022-02-09 11:20:47 -0500

    Adding in weather-app code

--- Project 2/playground/4-callback.js
+++ Project 2/playground/4-callback.js
@@ -1,20 +1,33 @@
-setTimeout(() => {
-    console.log('Two seconds are up')
-}, 2000)
+// setTimeout(() => {
+//     console.log('Two seconds are up')
+// }, 2000)
 
-const names = ['Ben', 'Andrew', 'Jen']
-const shortNames = names.filter((name) => {
-    return name.length <= 4
-})
+// const names = ['Ben', 'Andrew', 'Jen']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
 
-const geocode = (address, callback) => {
-    const data = {
-        latitude: 0,
-        longitude: 0
-    }
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
 
-    return data
+//         callback(data)
+//     }, 2000)
+// }
+
+// geocode('Philadelphia', (data) => {
+//     console.log(data)
+// })
+
+const add = (x, y, callback) => {
+    setTimeout(() => {
+        callback(x + y)
+    }, 2000)
 }
 
-const data = geocode('Philadelphia')
-console.log(data)
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
